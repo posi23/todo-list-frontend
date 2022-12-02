b0fc4594967a773d1f36f6d9cebf4d98dae2dfa9
Posi Adeyemi (ooa642) <ooa642@usask.ca>
2022-12-02 03:43:34 -0600

    added print statement for debugging purpose

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 
+console.log(process.env.API_URL);
 const API_URL = process.env.API_URL || 'http://localhost:8080/';
 
 const login = async (username: string, password: string) => {
